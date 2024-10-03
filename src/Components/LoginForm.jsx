efe5a35f99a1491545d6aaa89e0c7456509f8cdf
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2024-10-03 21:28:12 +0300

    add state and handler

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,30 +1,51 @@
+import { useState } from 'react';
 import { Button, Form, FormGroup, Input, Label } from 'reactstrap';
 
 export default function LoginForm() {
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+    isChecked: false,
+  });
+
+  function handleChange(e) {
+    const { name, value, type, checked } = e.target;
+
+    setForm({ ...form, [name]: type === 'checkbox' ? checked : value });
+  }
+
   return (
     <Form>
       <FormGroup>
-        <Label for="exampleEmail">Email</Label>
+        <Label for="maildata">Email</Label>
         <Input
-          id="exampleEmail"
+          id="maildata"
           name="email"
           placeholder="enter your email"
           type="email"
+          onChange={handleChange}
         />
       </FormGroup>
       <FormGroup>
-        <Label for="examplePassword">Password</Label>
+        <Label for="passworddata">Password</Label>
         <Input
-          id="examplePassword"
+          id="passworddata"
           name="password"
           placeholder="enter your password"
           type="password"
+          onChange={handleChange}
         />
       </FormGroup>
       <FormGroup check>
-        <Input type="checkbox" /> <Label check>I agree</Label>
+        <Input
+          type="checkbox"
+          onChange={handleChange}
+          name="isChecked"
+          checked={form.isChecked}
+        />{' '}
+        <Label check>I agree</Label>
       </FormGroup>
-      <Button>Submit</Button>
+      <Button disabled={!form.isChecked}>Submit</Button>
     </Form>
   );
 }
